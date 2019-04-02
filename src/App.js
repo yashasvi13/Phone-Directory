b3ca8cd107d602a283de12bc00c93a2943171366
yashasvi13 <yashasvij13@gmail.com>
2019-04-03 02:06:08 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AddContact from "./components/AddContact";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.addContact = this.addContact.bind(this);
+  }
+
+  state = {
+    contacts: [
+      {
+        name: "",
+        phone: ""
+      }
+    ]
+  };
+  addContact = contact => {
+    contact.id = Math.random();
+    let contacts = [...this.state.contacts, contact];
+    this.setState({
+      contacts: contacts
+    });
+  };
   render() {
     return (
       <BrowserRouter>
